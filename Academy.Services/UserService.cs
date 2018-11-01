3e6705139cb85487808c38ed9b3bc814bdbdae80
Ali Teber <ateber91@outlook.com>
2018-11-01 03:49:00 +0200

    Adding a few user services and validations.

---
+++ Academy.Services/UserService.cs
@@ -0,0 +1,118 @@
+﻿using Academy.Data;
+using Academy.DataContext;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Academy.Services
+{
+    public class UserService
+    {
+        private readonly AcademySiteContext context;
+
+        public UserService(AcademySiteContext context)
+        {
+            this.context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        public User GetUserById(int id)
+        {
+            Validations.RangeNumbers(0, int.MaxValue, id, "Your id can be only a postive number.");
+
+            return this.context.Users.Find(id);
+        }
+
+        public IEnumerable<Course> GetAllCourses()
+        {
+            return this.context.Courses;
+        }
+
+        public void UpdateRole(int userId, int newRoleId)
+        {
+            Validations.RangeNumbers(0, int.MaxValue, userId, "Your id can be only a postive number.");
+
+            if (userId == 1)
+            {
+                throw new InvalidOperationException("You are not allowed to set someone's role to Adminitrator");
+            }
+
+            var user = GetUserById(userId);
+
+            if (user == null)
+            {
+                throw new ArgumentNullException("User doesn't exist");
+            }
+            else
+            {
+                user.RoleId = newRoleId;
+            }
+
+            context.SaveChanges();
+        }
+
+        public void EvaluateStudent(int studentId, int assignmentId, int grade, int teacherId)
+        {
+
+            var teacher = this.context.Users.Include(us => us.TaughtCourses).FirstOrDefault(us => us.Id == teacherId);
+            var student = this.context.Users.Include(us => us.EnrolledStudents).Include(us => us.Grades).FirstOrDefault(us => us.Id == studentId);
+            var assaignment = this.context.Assignments.Include(c => c.Course).FirstOrDefault(a => a.Id == assignmentId);
+
+            if (assaignment == null)
+            {
+                throw new ArgumentNullException("Unfortunately there is no such an assignment");
+            }
+
+            if (teacher != null && assaignment.Course.TeacherId != teacher.Id)
+            {
+                throw new ArgumentException($"Teacher {teacher.UserName} is not assigned to {assaignment.Name}.");
+            }
+
+            if (student != null && student.EnrolledStudents.All(c => c.CourseId != assaignment.CourseId))
+            {
+                throw new ArgumentException($"Student {student.UserName} is not assigned to {assaignment.Name}.");
+            }
+
+            if (student.Grades.Any(g => g.AssignmentId == assaignment.Id))
+            {
+                throw new ArgumentException("Student already received grade for this assignment.");
+            }
+
+            var newGrade = new Grade
+            {
+                AssignmentId = assaignment.Id,
+                StudentId = student.Id,
+                ReceivedGrade = grade
+            };
+
+            student.Grades.Add(newGrade);
+            this.context.SaveChanges();
+        }
+
+        public void EnrollStudent(int studentId, string coursename)
+        {
+
+            var user = this.context.Users.Include(us => us.EnrolledStudents).FirstOrDefault(us => us.Id == studentId);
+            var course = this.context.Courses.FirstOrDefault(co => co.Name == coursename);
+
+            if (course == null)
+            {
+                throw new ArgumentOutOfRangeException("Unfortunately we are not offering such a course at the moment");
+            }
+            else if (user.EnrolledStudents.Any(es => es.CourseId == course.CourseId))
+            {
+                throw new ArgumentException($"You are already enrolled for the course {course.Name}.");
+            }
+            else
+            {
+                var enrolled = new EnrolledStudent
+                {
+                    StudentId = user.Id,
+                    CourseId = course.CourseId
+                };
+                user.EnrolledStudents.Add(enrolled);
+                this.context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ Academy.Services/Validations.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Academy.Services
+{
+    public static class Validations
+    {
+        public const int MIN_USERNAME = 3;
+        public const int MAX_USERNAME = 35;
+        public const int MIN_FULLNAME = 5;
+        public const int MAX_FULLNAME = 40;
+        public const int MIN_COURSENAME = 3;
+        public const int MAX_COURSENAME = 50;
+        public const int MIN_ASS = 3;
+        public const int MAX_ASS = 50;
+        public const int MIN_PASSWORD = 3;
+        public const int MAX_PASSWORD = 35;
+
+        public static void ValidateLength(int min, int max, string input, string message)
+        {
+            if (input.Length < min || input.Length > max)
+            {
+                throw new ArgumentOutOfRangeException(message);
+            }
+        }
+
+        public static void RangeNumbers(int min, int max, int id, string message)
+        {
+            if (id < min || id > max)
+            {
+                throw new ArgumentOutOfRangeException(message);
+            }
+        }
+
+        public static void VerifyUserName(string username)
+        {
+            Regex regex = new Regex(@"^[A-Za-z0-9]+(?:[_-][A-Za-z0-9]+)*$");
+            Match match = regex.Match(username);
+            if (!match.Success)
+            {
+                throw new ArgumentException("Invalid username");
+            }
+        }
+    }
+}
